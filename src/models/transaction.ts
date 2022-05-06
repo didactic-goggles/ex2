6f49afb5ba4bda241ad2d826bf627544d12dd3a6
didactic-goggles <58259532+didactic-goggles@users.noreply.github.com>
2022-05-06 20:37:56 +0300

    react ui init

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -1,6 +1,7 @@
 export interface ITransaction {
   id: string
   description: string
+  type: 'income' | 'excome'
   amount: number
   t_date: Date
 }
@@ -8,14 +9,19 @@ export interface ITransaction {
 export class Transaction {
   id: string
   description: string
+  type: 'income' | 'excome'
   amount: number
   t_date: Date
   constructor(data: ITransaction) {
     if (typeof data.amount !== 'number') {
       throw new Error('Invalid amount')
     }
+    if (data.type !== 'excome' && data.type !== 'income') {
+      throw new Error('Invalid type')
+    }
     this.id = data.id
     this.description = data.description
+    this.type = data.type
     this.amount = data.amount
     this.t_date = data.t_date
   }
